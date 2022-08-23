9f9ae356bcd9a69337ace2ee661d39abeda5470c
Evgeny Nikolaev <e.nikolaev@uplab.ru>
2022-08-23 15:21:18 +0300

    Организмы, слайдер новости

--- src/js/components/slider/index.js
+++ src/js/components/slider/index.js
@@ -1,9 +1,11 @@
 import SliderMainHero from './slider-main-hero';
 import SliderCard from './slider-card';
+import SliderNews from './slider-news';
 
 export default {
 	init() {
 		SliderMainHero.init();
 		SliderCard.init();
+		SliderNews.init();
 	},
 };
